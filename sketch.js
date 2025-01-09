bf5efd32fcd4202bc6da93729fa61ef822e6043b
Adira Sklar <149328146+AdiraSklar@users.noreply.github.com>
2025-01-09 17:15:34 -0500

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -21,13 +21,7 @@ function draw() {
     let min = minute();
     let sec = second();
     background(225);
-    textSize(32);
-    fill(180);
-    text(hr, 10, 30);
-    fill(100);
-    text(min, 10, 60);
-    fill(0);
-    text(sec, 10, 90);
+    
     drawGumballMachine(min);
     drawPlate();
     checkMinuteChange(min);
